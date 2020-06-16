ae6ba00fbb793c18f4bfc4aad277b08ca166dbd8
heqiming <heqiming@sd.chinamobile.com>
2020-06-16 21:52:12 +0800

    .

--- src/org/ayakaji/reverse/thirdparty/IngressUemUpdate.java
+++ src/org/ayakaji/reverse/thirdparty/IngressUemUpdate.java
@@ -25,9 +25,12 @@ public class IngressUemUpdate {
 	private static String K8s_Master_Pass = "Jnydzycsyy@123";
 	private static Connection conn = null; // 全局连接
 	private static String K8s_Master_Trans_Folder = "/home/heqiming"; // Temporary Transmit Folder
-	private static String Uem_Err_Src_Full_Path = "D:\\workspaces\\uem\\uem-err-1.5.js"; // 原始路径
+	private static String Uem_Err_Src_Full_Path = "D:\\workspaces\\uem\\uem-err-1.5.js"; // 业务异常截获脚本
+	private static String Uem_JS_Err_Full_Path = "D:\\workspaces\\uem\\uem-jserr-1.5.js"; // 截获js错误的脚本
 	private static String Uem_Err_Filename = "uem-err-1.5.js"; // 源文件实际文件名
+	private static String Uem_JS_Err_Filename = "uem-jserr-1.5.js"; // 截获js错误的脚本
 	private static String Uem_Err_Ctnr_Path = "/usr/local/nginx/html/uem/uem-err-1.5.js"; // 容器中的路径
+	private static String Uem_JS_Err_Ctnr_Path = "/usr/local/nginx/html/uem/uem-jserr-1.5.js"; // 容器中的路径
 	private static String Json2_Src_Full_Path = "D:\\workspaces\\uem\\json2.js"; // 原始路径
 	private static String Json2_Filename = "json2.js"; // 源文件实际文件名
 	private static String Json2_Ctnr_Path = "/usr/local/nginx/html/uem/json2.js"; // 容器中的路径
@@ -47,16 +50,33 @@ public class IngressUemUpdate {
 			+ " | grep nginx-ingress | head -1 | awk '{print $1}'";
 	private static String Ngx_Pod_Name = null;
 	private static String Mrk_Ngx_Uem = "UEM 1.5 Configurations"; // UEM是否已注入Nginx的标志物
-	private static String Ngx_Uem_Cnf = "\n\t\t##########################\n" + "\t\t# UEM 1.5 Configurations #\n"
-			+ "\t\t# Begin                  #\n" + "\t\t##########################\n" + "\t\t\n"
-			+ "\t\t# Receive Uploaded UEM Data\n" + "\t\tlocation /uem/uem-1.5.do {\n"
-			+ "\t\t\tdefault_type text/html;\n" + "\t\t\treturn 200 'UEM uploaded successfully!';\n" + "\t\t}\n"
-			+ "\t\t\n" + "\t\t# Main Injected Agents\n" + "\t\t# 4xx, 5xx Pages\n" + "\t\tlocation /uem/ {\n"
-			+ "\t\t\troot html;\n" + "\t\t}\n" + "\t\t\n" + "\t\t# Inject Agents in each page\n"
+	private static String Ngx_Uem_Cnf =
+			"\n\t\t##########################\n"
+			+ "\t\t# UEM 1.5 Configurations #\n"
+			+ "\t\t# Begin                  #\n"
+			+ "\t\t##########################\n"
+			+ "\t\t\n"
+			+ "\t\t# Receive Uploaded UEM Data\n"
+			+ "\t\tlocation /uem/uem-1.5.do {\n"
+			+ "\t\t\tdefault_type text/html;\n"
+			+ "\t\t\treturn 200 'UEM uploaded successfully!';\n"
+			+ "\t\t}\n"
+			+ "\t\t\n"
+			+ "\t\t# Main Injected Agents\n"
+			+ "\t\t# 4xx, 5xx Pages\n"
+			+ "\t\tlocation /uem/ {\n"
+			+ "\t\t\troot html;\n"
+			+ "\t\t}\n"
+			+ "\t\t\n"
+			+ "\t\t# Inject Agents in each page\n"
 			+ "\t\tsub_filter_types *;\n"
 			+ "\t\tsub_filter \"</body>\" \"<script type='text/javascript' src='/uem/uem-err-1.5.js'></script></body>\";\n"
-			+ "\t\tsub_filter_once off;\n" + "\t\t\n" + "\t\t##########################\n"
-			+ "\t\t# UEM 1.5 Configurations #\n" + "\t\t# End                    #\n"
+			+ "\t\tsub_filter \"<head>\" \"<head><script type='text/javascript' src='/uem/uem-jserr-1.5.js'></script>\";\n"
+			+ "\t\tsub_filter_once off;\n"
+			+ "\t\t\n"
+			+ "\t\t##########################\n"
+			+ "\t\t# UEM 1.5 Configurations #\n"
+			+ "\t\t# End                    #\n"
 			+ "\t\t##########################\n";
 	private static String Mrk_Err_Page = "error_page 404 /uem/4xx.html;";
 
@@ -84,6 +104,8 @@ public class IngressUemUpdate {
 			+ " " + K8s_Ingress_Tenant + "/" + Ngx_Pod_Name + ":" + Ngx_Ing_Conf_Ctnr_Path;
 	private static String Push_Uem_Err = kubectl + "cp " + K8s_Master_Trans_Folder + "/" + Uem_Err_Filename + " "
 			+ K8s_Ingress_Tenant + "/" + Ngx_Pod_Name + ":" + Uem_Err_Ctnr_Path;
+	private static String Push_Js_Err = kubectl + "cp " + K8s_Master_Trans_Folder + "/" + Uem_JS_Err_Filename + " "
+			+ K8s_Ingress_Tenant + "/" + Ngx_Pod_Name + ":" + Uem_JS_Err_Ctnr_Path;
 	private static String Push_Json2 = kubectl + "cp " + K8s_Master_Trans_Folder + "/" + Json2_Filename + " "
 			+ K8s_Ingress_Tenant + "/" + Ngx_Pod_Name + ":" + Json2_Ctnr_Path;
 	private static String Push_4xx = kubectl + "cp " + K8s_Master_Trans_Folder + "/" + Fourxx_Zip_Filename + " "
@@ -105,6 +127,7 @@ public class IngressUemUpdate {
 //		deployNgxConf();
 		deployUEM();
 //		deploy4xxPages();
+//		deployJSErr();
 	}
 
 	private static void deploy() throws IOException, InterruptedException {
@@ -134,6 +157,7 @@ public class IngressUemUpdate {
 		SshUtil.upload(conn, Ngx_Ing_Conf_Full_Path, K8s_Master_Trans_Folder);
 		// 7、上传UEM代理到Master节点中转目录
 		SshUtil.upload(conn, Uem_Err_Src_Full_Path, K8s_Master_Trans_Folder);
+		SshUtil.upload(conn, Uem_JS_Err_Full_Path, K8s_Master_Trans_Folder);
 		// 8、上传json2依赖到Master节点中转目录
 		SshUtil.upload(conn, Json2_Src_Full_Path, K8s_Master_Trans_Folder);
 		// 9、上传默认4xx.html到Master节点中转目录
@@ -144,19 +168,21 @@ public class IngressUemUpdate {
 		SshUtil.execCommand(conn, Create_Uem_Folder);
 		// 12、推送UEM脚本到容器
 		SshUtil.execCommand(conn, Push_Uem_Err);
-		// 13、推送Json2脚本到容器
+		// 13、推送JS_ERR脚本到容器
+		SshUtil.execCommand(conn, Push_Js_Err);
+		// 14、推送Json2脚本到容器
 		SshUtil.execCommand(conn, Push_Json2);
-		// 14、推送4xx.zip到容器
+		// 15、推送4xx.zip到容器
 		SshUtil.execCommand(conn, Push_4xx);
-		// 15、解压缩4xx.zip
+		// 16、解压缩4xx.zip
 		SshUtil.execCommand(conn, Unzip_4xx);
-		// 16、设置属性755
+		// 17、设置属性755
 		SshUtil.execCommand(conn, Chmod_Uem);
-		// 17、验证Nginx配置
+		// 18、验证Nginx配置
 		SshUtil.execCommand(conn, Ngx_Ing_Cnf_Verify);
-		// 18、重新加载Nginx配置
+		// 19、重新加载Nginx配置
 		SshUtil.execCommand(conn, Ngx_Ing_Cnf_Reload);
-		// 19、关闭Connection
+		// 20、关闭Connection
 		conn.close();
 	}
 
@@ -174,10 +200,28 @@ public class IngressUemUpdate {
 		conn.close();
 	}
 
+	/**
+	 * Deploy uem-err-1.5.js
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
 	private static void deployUEM() throws IOException, InterruptedException {
 		SshUtil.upload(conn, Uem_Err_Src_Full_Path, K8s_Master_Trans_Folder);
 		SshUtil.execCommand(conn, Create_Uem_Folder);
 		SshUtil.execCommand(conn, Push_Uem_Err);
+		SshUtil.execCommand(conn, Chmod_Uem);
+		conn.close();
+	}
+	
+	/**
+	 * Deploy uem-jserr-1.5.js
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	private static void deployJSErr() throws IOException, InterruptedException {
+		SshUtil.upload(conn, Uem_JS_Err_Full_Path, K8s_Master_Trans_Folder);
+		SshUtil.execCommand(conn, Create_Uem_Folder);
+		SshUtil.execCommand(conn, Push_Js_Err);
 		SshUtil.execCommand(conn, Chmod_Uem);
 		conn.close();
 	}
